14a003e08f0fdd196290220feb36bd0a21f75cfc
Mandloi <pmandloi@Prateek-Mac.local>
2016-07-02 17:11:32 +0530

    Typeof start

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,26 @@
-var pId = 1234;
-console.log(pId);
+/* typeof */
+var pid = 1234;
+console.log(typeof pid);
 
-var pId = "PaymentID : 1234";
-pId = 12321;
-console.log(pId);
+var sid = "SID: 1234";
+// console.log(typeof sid);
 
-newpId = "10101";
-console.log(newpId);
+var isCool = true;
+// console.log(typeof isCool);
+
+var isLunchTime;
+// console.log(typeof isLunchTime);
+
+var obj = {
+	workshop: "JavaScript"
+};
+// console.log(typeof obj);
+
+var conferenceRooms = ["Maharashtra", "Gujrat", "Punjab", "Tamil Nadu"];
+// console.log(typeof conferenceRooms);
+
+var supplyFood = null;
+// console.log(typeof supplyFood);
+
+function getData() {}
+// console.log(typeof getData);
