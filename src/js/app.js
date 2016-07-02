210a89ef1d0974c24af2d30e65431fbd0176058b
Mandloi <pmandloi@Prateek-Mac.local>
2016-07-02 18:30:28 +0530

    Conditional statements & loops start

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,7 @@
-/* Simple Function Definition */
-function helloFoo() {
-	console.log("Foo function invoked.");
+/* if begins */
+var orderTotal = 49.99;
+var isAffordable = false;
+if(orderTotal < 50) {
+	isAffordable = true;
 }
-helloFoo();
-
-/* Parameterised Function */
-function helloFooParam(param1) {
-	console.log("Foo function invoked with : " + param1);
-}
-helloFooParam(10);
-
-/* Calling Function Before Declaration */
-callFooBeforeDeclaring();
-
-function callFooBeforeDeclaring() {
-	console.log("Foo still invoked, irrespective of where it is declared.");
-}
-
-/* Variable function arguments */
-function fooWithVarArgs(company, bu, event) {
-	console.log("Event is : " + event);
-}
-fooWithVarArgs("Intuit", "SBG");
-
-/* Function with return keyword */
-function add(num1, num2) {
-	return num1 + num2;
-}
-var result = add(3,5);
-console.log(result);
-
-/* Functions without return keyword, default return type */
-function withoutReturn() {
-	/* does something */
-}
-var result = withoutReturn();
-console.log(result);
-
-/* Anonymous function */
-var task = function() {
-	console.log("Complex tasks executed..."); 
-};
-console.log(typeof task);
-task();			
-
-
+console.log(isAffordable);
