965949f9007a14b280e691ba59887d2aaef460ea
Mandloi <pmandloi@Prateek-Mac.local>
2016-07-02 17:48:47 +0530

    Functions start

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,8 @@
-/* typeof */
-var pid = 1234;
-console.log(typeof pid);
+/* Simple Function Definition */
+function helloFoo() {
+	console.log("Foo function invoked.");
+}
+helloFoo();
+			
 
-var sid = "SID: 1234";
-console.log(typeof sid);
 
-var isCool = true;
-console.log(typeof isCool);
-
-var isLunchTime;
-console.log(typeof isLunchTime);
-
-var obj = {
-	workshop: "JavaScript"
-};
-console.log(typeof obj);
-
-var conferenceRooms = ["Maharashtra", "Gujrat", "Punjab", "Tamil Nadu"];
-console.log(typeof conferenceRooms);
-
-var supplyFood = null;
-console.log(typeof supplyFood);
-
-function getData() {}
-console.log(typeof getData);
