204dbb9dea7ecc6b2320b0fc0414ff4f419f6231
Mandloi <pmandloi@Prateek-Mac.local>
2016-07-02 17:15:07 +0530

    Typeof end

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,24 @@ var pid = 1234;
 console.log(typeof pid);
 
 var sid = "SID: 1234";
-// console.log(typeof sid);
+console.log(typeof sid);
 
 var isCool = true;
-// console.log(typeof isCool);
+console.log(typeof isCool);
 
 var isLunchTime;
-// console.log(typeof isLunchTime);
+console.log(typeof isLunchTime);
 
 var obj = {
 	workshop: "JavaScript"
 };
-// console.log(typeof obj);
+console.log(typeof obj);
 
 var conferenceRooms = ["Maharashtra", "Gujrat", "Punjab", "Tamil Nadu"];
-// console.log(typeof conferenceRooms);
+console.log(typeof conferenceRooms);
 
 var supplyFood = null;
-// console.log(typeof supplyFood);
+console.log(typeof supplyFood);
 
 function getData() {}
-// console.log(typeof getData);
+console.log(typeof getData);
