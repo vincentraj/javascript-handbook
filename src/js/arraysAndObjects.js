b9f0119d5c582eacef2f34eaeea38d538f3fbdb4
Mandloi <pmandloi@Prateek-Mac.local>
2016-07-12 10:44:39 +0530

    Arrays and Objects end

--- src/js/arraysAndObjects.js
+++ src/js/arraysAndObjects.js
@@ -31,3 +31,76 @@ updateInvoice(invoice);
 
 console.log(invoice.receiverName);
 
+/* Arrays Intro - creation and loosly typed */
+var empIds = new Array(1, 2, 8, 3, 1, 13);
+console.log(empIds);
+
+/* Array objects */
+/* immutable functions */
+console.log("Length: ", empIds.length);
+console.log("toString(): ", empIds.toString());
+console.log("valueOf(): ", empIds.valueOf(2));
+console.log("join(): ", empIds.join("/"));
+
+var coOp = [3, 5];
+empIds = empIds.concat(coOp);
+console.log("concat(): ", empIds);
+
+var sliced = empIds.slice(0,2);
+console.log("slice(): ", sliced);
+console.log("empIds after slice(): ", empIds);
+
+console.log("indexOf(): ", empIds.indexOf(1));
+console.log("lastIndexOf(): ", empIds.lastIndexOf(1));
+
+console.log("map(): ", empIds.map(function(val) { return val*5; }));
+
+/* mutators */
+empIds.push(1);
+console.log("push(): ", empIds);
+console.log("pop(): ", empIds.pop());
+
+empIds.shift();
+console.log("shift(): ", empIds);
+console.log("reverse(): ", empIds.reverse());
+
+var spliced = empIds.splice(1,1);
+console.log("splice(): ", spliced);
+console.log("empIds after splice(): ", empIds);
+
+empIds.sort();
+console.log("sort() 1: ", empIds);
+empIds.sort(function(a, b) {
+	return a - b;
+});
+console.log("sort() 2: ", empIds);
+
+/* RegEx */
+var text = "I am new to Amsterdam!!!";
+var pattern = new RegExp("am", "g");
+console.log(pattern.test(text));
+
+pattern = /am/ig;
+console.log(pattern.exec(text));
+console.log(text.match(pattern));
+
+/* Objects */
+var invoiceObj = Object.create(invoice);
+console.log(invoiceObj);
+
+function MyJavaScriptObject() {
+
+}
+
+if (typeof MyJavaScriptObject.create !== 'function') {
+	MyJavaScriptObject.create = function (o) {         
+		var MyJSObject = function () {};   
+		MyJSObject.prototype = o;       
+		return new MyJSObject();  
+	};
+}
+
+var myObject = MyJavaScriptObject.create(invoice);
+console.log(myObject);
+
+/* JSON */
